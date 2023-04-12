b36f2fef61afa8137cd2e497115dea63f78c1fea
am0654 <94629609+am0654@users.noreply.github.com>
2023-04-13 02:06:44 +0530

    Create ReverseAString.cpp

---
+++ ReverseAString.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    char str[200], strTemp[200];
+    int len, i=0;
+    cout<<"Enter the String: ";
+    cin>>str;
+    while(str[i]!='\0')
+        i++;
+    len = i;
+    strTemp[len] = '\0';
+    len--;
+    i = 0;
+    while(str[i]!='\0')
+    {
+        strTemp[len] = str[i];
+        i++;
+        len--;
+    }
+    i=0;
+    while(strTemp[i]!='\0')
+    {
+        str[i] = strTemp[i];
+        i++;
+    }
+    cout<<"\nReverse = "<<str;
+    cout<<endl;
+    return 0;
+}
+
+Input:
+Enter the String: welcome
+
+Output:
+Reverse = emoclew
