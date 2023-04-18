b2b1ee07b6a49ac886f94739dea0e2409cff37cf
am0654 <94629609+am0654@users.noreply.github.com>
2023-04-18 23:22:57 +0530

    Create UppercaseToLowercase.cpp

---
+++ UppercaseToLowercase.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char chUpper, chLower;
+    int ascii;
+    cout<<"Enter the Character (in Uppercase): ";
+    cin>>chUpper;
+    ascii = chUpper;
+    ascii = ascii+32;
+    chLower = ascii;
+    cout<<"\nIts Lowercase: "<<chLower;
+    cout<<endl;
+    return 0;
+}
+
+
+Input:
+Enter the Character (in Uppercase): A
+Output:
+Its Lowercase: a
